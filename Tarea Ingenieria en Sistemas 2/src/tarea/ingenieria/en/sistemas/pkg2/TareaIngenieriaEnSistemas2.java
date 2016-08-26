8ea641f3d636045552bcef3402b8f3381f3c0c8b
Jorge Mero <Jorge Mero@JorgeMero>
2016-08-26 12:16:32 -0500

    2 commit

--- Tarea Ingenieria en Sistemas 2/src/tarea/ingenieria/en/sistemas/pkg2/TareaIngenieriaEnSistemas2.java
+++ Tarea Ingenieria en Sistemas 2/src/tarea/ingenieria/en/sistemas/pkg2/TareaIngenieriaEnSistemas2.java
@@ -5,8 +5,12 @@
  */
 package tarea.ingenieria.en.sistemas.pkg2;
 
+import java.util.Scanner;
+
 /**
  *
+ *  Descripciòn: Validar un Nombre de Usuario y una Clave
+ *  se le permite tres intentos antes de bloquear el sistema 
  * @author Jorge Mero
  */
 public class TareaIngenieriaEnSistemas2 {
@@ -15,8 +19,41 @@ public class TareaIngenieriaEnSistemas2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bienvenido a java");
         
+        Scanner mientrada=new Scanner(System.in);
+        String clave="112346578",usuario="JORGE";
+        String miclave="",miusuario="";
+        int contador=0, bandera=0;
+        do { //todo lo q se repite
+            
+            //pido un nombre de usuario
+            System.out.println("Ingrese su nombre de usuario");
+            miusuario=mientrada.nextLine();
+            //pido una clave
+            System.out.println("Ingrese su clave");
+            miclave=mientrada.nextLine();
+            //cuento los intento q van
+            contador=contador+1;
+            //compara si el nombre de usuario y la clave son correcto
+            if ((clave.equals(miclave))&& (usuario.equals(miusuario))){
+                 bandera=1;
+            }
+            else {
+                //mostrar el mensaje de clave y usuario incorecta 
+                //muestro cuanto intento llevo
+                System.out.printf("Nombre de usario o clave incorrecta. Van %d de 3 intentos\n",contador);
+            }
+            if (contador==3)  {//ya llego el nùmero màximo de intento
+                bandera=1; //ya no puede repetir
+            }
+        } while (bandera==0);
+        if ((clave.equals(miclave))&& (usuario.equals(miusuario))){
+            System.out.println("Bienvenido al sistema");
+        }
+        else {
+            System.out.println("Sistema bloqueado");
+        } 
     }
-    
 }
+        
+
